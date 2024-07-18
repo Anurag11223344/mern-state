3073ac13d1f271a177289b8fd713131e7a0fa152
Anurag11223344 <anuragsrivastava48946001@gmail.com>
2024-07-18 05:43:15 +0530

    Mongodb Connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+const username = 'Anurag';
+const password = encodeURIComponent('Anurag@123'); // Encode the password
+const cluster = 'mern-estate.9d7kpch.mongodb.net';
+const dbname = 'mern-estate';
+
+mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority&appName=${dbname}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
 const app = express();
 
 app.listen(3000, () => {
