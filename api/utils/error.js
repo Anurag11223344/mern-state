72fb86fdcf26ee167159b6323f055b40beb5060b
Anurag11223344 <anuragsrivastava48946001@gmail.com>
2024-08-06 05:48:53 +0530

    Changed Comment and removed semicolon

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,7 +1,7 @@
-//Insaan galti kre toh error show karega
+//User galti kre toh error show karega
 export const errorHandler = (statusCode, message) => {
-    const error = new Error(message);
-    error.statusCode = statusCode;
-    error.message = message;
+    const error = new Error(message)// javaScript object(Error constructor)
+    error.statusCode = statusCode
+    error.message = message
     return error;
 }
