8d8e73056597724e88a863cd7e2f3fae8bd3f232
Anurag11223344 <anuragsrivastava48946001@gmail.com>
2024-08-25 16:39:30 +0530

    added space

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,15 +51,19 @@ export default function SignUp() {
       <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
 
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+      
         <input type="text" placeholder='username' 
         className='border p-3 rounded-lg' id='username' 
         onChange={handleChange} />
+
         <input type="email" placeholder='email' 
         className='border p-3 rounded-lg' id='email' 
         onChange={handleChange} />
+
         <input type="password" placeholder='password' 
         className='border p-3 rounded-lg' id='password' 
         onChange={handleChange} />
+
         <button disabled={loading} className='bg-slate-700 text-white p-3
         rounded-lg uppercase hover:opacity-95
         disabled:opacity-80'>
