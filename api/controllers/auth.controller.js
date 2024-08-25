c46c272838d1ff76a81de3b2f556a826427ef249
Anurag11223344 <anuragsrivastava48946001@gmail.com>
2024-08-25 17:42:17 +0530

    Created sign in api route and added env configuration

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,32 @@
-import { log } from "mathjs";
+
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     //Info we get from the browser
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const isEmailPresnet = await User.findOne({email: email})
+    const isUserPresnet = await User.findOne({username: username})
+
+    if (isUserPresnet) {
+        return res
+            .status(410)
+            .json({
+                message: "Username already exists",
+            })
+    }
+    if (isEmailPresnet) {
+        return res
+            .status(410)
+            .json({
+                message: "Email already exists",
+            })
+    }
+
     const newUser = new User({ username, email, password: hashedPassword });
     try{
         await newUser.save()
@@ -17,3 +37,22 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+//SignIn Api route making
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try{
+        const validUser = await User.findOne({ email });
+        if(!validUser) return next(errorHandler(404, 'User not found')); //yeh check krega ki signin ke time pr joh email daala hai user ne woh correct hai ki nhi.
+        const validPassword = bcryptjs.compareSync(password, validUser.password);//yeh password check krega....but we have hashed password in the database so we have to compare the hashed password with the password that user has entered.
+        if(!validPassword) return next(errorHandler(400, 'Wrong credentials!'));// agr glt hua.. toh error show krega
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: Pass, ...rest } = validUser._doc;
+        res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } catch (error){
+        next(error);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+
+dotenv.config();
 
 const username = 'Anurag';
 const password = encodeURIComponent('Anurag@123'); // Encode the password
