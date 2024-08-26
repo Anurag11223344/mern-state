130dd45c87e1fd5b91402e5d9ec648a31d6264e6
Anurag11223344 <anuragsrivastava48946001@gmail.com>
2024-08-26 08:27:57 +0530

    Added Redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {store} from './redux/store.js';
-import {Provider} from 'react-redux';
+import { persistor, store} from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'; // after signin...user ki info. page ke redux me rhe.....refresh krne ke baad htt na jaye
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
+    <PersistGate loading = {null} persistor = {persistor}>
     <App />
+    </PersistGate>
   </Provider>,
 )
