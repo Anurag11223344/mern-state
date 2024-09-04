1a9c2a34e99f31352b460a967c9b3a0deaab5d51
Anurag11223344 <anuragsrivastava48946001@gmail.com>
2024-09-04 11:56:13 +0530

    Completed Page-listing UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 
 export default function App() {
@@ -17,8 +18,9 @@ export default function App() {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/about" element={<About />} />
-      <Route element={<PrivateRoute />}>
+      <Route element={<PrivateRoute />}>{/* if user is not authenticated then he/she will not be able to see the profile page */}
         <Route path="/profile" element={<Profile />} />
+        <Route path="/create-listing" element={<CreateListing />} />
       </Route>
       <Route path="/profile" element={<Profile />} />
       
