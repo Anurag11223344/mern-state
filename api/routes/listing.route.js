3343779b6b3336768eba55a10b14dcf804bd92de
Anurag Srivastava <anuragsrivastava48946001@gmail.com>
2025-02-05 00:31:25 +0530

    Added the route and done necessary changes.

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing); // now create listing.controller.js. // verifyToken coz-> we have to check that the person is authenticated or not.
 router.delete('/delete/:id', verifyToken, deleteListing); //Route for delete functionality.....when we click delete on the profile page....after that the listing should be deleted // verifyToken coz-> we have to check that the person is authenticated or not.
 router.post('/update/:id', verifyToken, updateListing); //Route for update functionality.....when we click edit on the profile page....after that the listing should be updated // verifyToken coz-> we have to check that the person is authenticated or not.
+router.get('/get/:id', getListing);
 
 export default router;
